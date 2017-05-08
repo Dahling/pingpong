2f91b3035e07d001d6201b353051b1badddefd39
Jenna Cooper <jchristinec@gmail.com>
2017-05-07 22:10:08 -0700

    Old folder wouldn't push to GitHub, so I had to create a new one

---
+++ README.md
@@ -0,0 +1,42 @@
+# Ping Pong
+
+## Created by Jenna Cooper
+
+
+## User Story
+
+*
+
+
+## Specs
+
+| Objective | Example: Input | Example: Output |
+|:-------------:|:-------------:|:-------------:|
+| It can count up to the provided number | 2 | [1,2] |
+|  |  |  |
+|  |  |  |
+|  |  |  |
+
+
+## Setup/Installation Requirements
+
+  * Clone the repository in GitHub
+
+
+
+
+## Bugs
+There are no known bugs at this time, but please contact the creator with questions or concerns regarding this application.
+
+
+## Technologies Used
+
+  * jQuery
+  * CSS - Bootstrap
+  * HTML
+
+
+## Licensing
+This application features MIT licensing.
+
+Copyright &copy; 2017 **Jenna Cooper** All Rights Reserved.
---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+
+// Buissnes
+var endDisplay = function(givenNumber){
+  var theArray = [], count = 0;
+  if (isNaN(givenNumber)) {
+    return theArray;
+  }
+  else {
+    for (var i = 0; i < givenNumber; i++){
+        count++;
+        theArray.push(count);
+    }
+    for (var i = 0; i < theArray.length; i++){
+      if (theArray[i] % 15 === 0){
+        theArray[i] = "pingpong";
+      }else if (theArray[i] % 5 === 0){
+        theArray[i] = "pong";
+      }else if (theArray[i] % 3 === 0){
+        theArray[i] = "ping";
+      }
+    } return theArray;
+  }
+};
+
+// User-interface
+$(document).ready(function(){
+  $("form#main").submit(function(event){
+    event.preventDefault();
+    $("#output").empty();
+    var givenInput = parseInt($("input#number").val())
+    var endArray = endDisplay(givenInput);
+    for (var i = 0; i < endArray.length; i++){
+      $("#output").append("<li>" + endArray[i] + "</li>");
+    }
+
+  });
+});
